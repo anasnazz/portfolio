040f4bb1081f19252aa766da7bcd74c0a4474a85
Anas <115024720+anasnazz@users.noreply.github.com>
2024-06-23 09:03:26 +0000

    Initial setup of client-side code

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import RoutesComp from './routes';
+
+function App() {
+  return (
+    <main>
+      <RoutesComp/>
+    </main>
+  );
+}
+
+export default App;
---
+++ client/src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home/Home'
+import Projects from './pages/projects/Projects'
+import Contact from './pages/contact/Contact'
+
+function RoutesComp() {
+  return (
+    <Routes>
+        <Route path='/' element={<Home/>}></Route>
+        <Route path='/projects' element={<Projects/>}></Route>
+        <Route path='/contact' element={<Contact/>}></Route>
+    </Routes>
+  )
+}
+
+export default RoutesComp
